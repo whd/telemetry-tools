91912e1e9bf32abc66a22c96c9d5e705714dfcd4
Mark Reid <mreid@mozilla.com>
2015-11-03 16:32:08 -0400

    Faster listing of high-cardinality buckets.

--- telemetry/util/test_s3.py
+++ telemetry/util/test_s3.py
@@ -23,6 +23,8 @@ class FakeKey:
 class FakeBucket:
   def __init__(self, filenames):
     self.filenames = filenames
+  def __str__(self):
+    return "A fake bucket"
 
   def list(self, prefix='', delimiter=None):
     dirs = set()
@@ -38,6 +40,24 @@ class FakeBucket:
     for d in dirs:
       yield FakeKey(d)
 
+# A bucket that throws an exception if you try to list anything other than
+# the given prefixes. This lets us validate that we are iterating by lists of
+# allowed values instead of listing bucket contents first.
+class UnlistableBucket:
+  def __init__(self):
+    pass
+  def __str__(self):
+    return "A fake bucket that doesn't like listing"
+
+  def list(self, prefix='', delimiter=None):
+    if prefix == "d1a/d2a/" or prefix == "d1a/d2b/":
+      yield FakeKey("{}1".format(prefix))
+      yield FakeKey("{}2".format(prefix))
+      yield FakeKey("{}3".format(prefix))
+    else:
+      # Raise an exception if user tries to iterate unexpectedly.
+      raise Exception("bad prefix: {}".format(prefix))
+
 v2bucket = FakeBucket([
   "saved_session/Fennec/nightly/26.0a1/20130806030203.20131021.v2.log.8b30fadcf5b84df8b860bce47a23146a.lzma",
   "saved_session/Firefox/release/24.0/20130910160258.20131002.v2.log.264b07580df349678b1247d13ea2e6f3.lzma",
@@ -52,6 +72,7 @@ v4bucket = FakeBucket([
   "20150903/telemetry/4/saved_session/Firefox/release/24.0/20130910160258/20150903051633.482_ip-172-31-16-184",
   "20150903/telemetry/4/saved_session/Firefox/release/24.0/20130910160258/20150903051644.482_ip-172-31-16-184",
   "20150903/telemetry/4/saved_session/Firefox/release/24.0/20130910160258/20150903051655.482_ip-172-31-16-184",
+  "20150903/telemetry/4/saved_session/Firefox/release/25.0/20130910160258/20150903051655.482_ip-172-31-16-184",
   "20150903/telemetry/4/saved_session/Fennec/nightly/26.0a1/20130806030203/20150903051626.482_ip-172-31-16-184"])
 
 v4execbucket = FakeBucket([
@@ -161,7 +182,7 @@ def test_v4schema():
         },
         {
           "field_name": "appVersion",
-          "allowed_values": "24.0"
+          "allowed_values": {"max": "24.0"}
         },
         {
           "field_name": "appBuildId",
@@ -221,10 +242,83 @@ def test_v4execschema():
     assert("a/b/c/d/20150901/20150901223019.579_ip-172-31-16-184" in found)
     assert("a/b/c/d/20150901/20150901224519.623_ip-172-31-16-184" in found)
 
+# For a high-cardinality bucket, we want to ensure that we don't list the full
+# set of files, and iterate through only the specific values given.
+def test_high_cardinality():
+    schema_spec = {
+      "version": 2,
+      "dimensions": [
+        {
+          "field_name": "dimension1",
+          "allowed_values": "d1a"
+        },
+        {
+          "field_name": "dimension2",
+          "allowed_values": ["d2a", "d2b"]
+        }
+      ]
+    }
+    schema = TelemetrySchema(schema_spec)
+
+    b = UnlistableBucket()
+    found = set()
+    for f in s3util.list_heka_partitions(b, schema=schema):
+      found.add(f.name)
+    assert(len(found) == 6)
+    assert("d1a/d2a/1" in found)
+    assert("d1a/d2a/2" in found)
+    assert("d1a/d2a/3" in found)
+    assert("d1a/d2b/1" in found)
+    assert("d1a/d2b/2" in found)
+    assert("d1a/d2b/3" in found)
+
+    # make sure old-style listing triggers the exception
+    found = set()
+    threw = False
+    msg = None
+    try:
+      for f in s3util.list_partitions(b, schema=schema, include_keys=True, dirs_only=True):
+        found.add(f.name)
+    except Exception as e:
+      threw = True
+      msg = e.message
+
+    assert(threw)
+    assert(msg == "bad prefix: ")
+
+def test_list_prefixes():
+    found = set()
+    for f in s3util._list_prefixes(v4bucket):
+      found.add(f.name)
+
+    assert(len(found) == 1)
+    assert("20150903/" in found)
+
+    found = set()
+    for f in s3util._list_prefixes(v4bucket, prefix='20150903/telemetry/4/saved_session/'):
+      found.add(f.name)
+
+    assert(len(found) == 2)
+    assert("20150903/telemetry/4/saved_session/Fennec/" in found)
+    assert("20150903/telemetry/4/saved_session/Firefox/" in found)
+
+def test_list_files():
+    found = set()
+    for f in s3util._list_files(v4bucket, prefix='20150903/telemetry/4/saved_session/Firefox/release/24.0/20130910160258/'):
+      found.add(f.name)
+
+    assert(len(found) == 3)
+    assert("20150903/telemetry/4/saved_session/Firefox/release/24.0/20130910160258/20150903051633.482_ip-172-31-16-184" in found)
+    assert("20150903/telemetry/4/saved_session/Firefox/release/24.0/20130910160258/20150903051644.482_ip-172-31-16-184" in found)
+    assert("20150903/telemetry/4/saved_session/Firefox/release/24.0/20130910160258/20150903051655.482_ip-172-31-16-184" in found)
+
 def main():
     test_v2schema()
+    test_list_prefixes()
+    test_list_files()
     test_v4schema()
     test_v4execschema()
+    test_high_cardinality()
     return 0
 
 if __name__ == "__main__":
